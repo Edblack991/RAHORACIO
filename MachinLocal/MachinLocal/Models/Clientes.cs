fe4d9dcf35c9a22086af49c19e7d4eccf0bad941
Gustavo Flores <gustavo_nkf@hotmail.com>
2018-10-25 07:41:37 -0500

    Coo mama el pili ya saquenlo del equipo

---
+++ MachinLocal/MachinLocal/Models/Clientes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MachinLocal.Models
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+
+    }
+}
