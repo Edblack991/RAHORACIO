e918f931ba180e651fb1493224ad6d5c3cb0bd98
Gustavo Flores <gustavo_nkf@hotmail.com>
2018-10-25 09:44:20 -0500

    1.6

--- MachinLocal/MachinLocal/Models/Servicios.cs
+++ MachinLocal/MachinLocal/Models/Servicios.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,19 @@ namespace MachinLocal.Models
 {
     public class Servicios
     {
+        [Key]
+        public int ServicioID { get; set; }
+
+        [Required(ErrorMessage = "Es necesario introducir el nombre del servicio")]
+        [StringLength(50, ErrorMessage = "Solo se pueden introducior 50 caracteres")]
+        public String NombreServicio { get; set; }
+
+        [Required(ErrorMessage = "Es necesario introducir la descripcion")]
+        [StringLength(100, ErrorMessage = "Solo se pueden introducior 100 caracteres")]
+        public String Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Es necesario introducir el Costo")]
+        [StringLength(8, ErrorMessage = "Solo se pueden introducior 8 caracteres")]
+        public int Costo { get; set; }
     }
 }
