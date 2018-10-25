4944fb3d52a2ddc66bb7f8e0422e30ff8361fe09
Gustavo Flores <gustavo_nkf@hotmail.com>
2018-10-25 10:15:31 -0500

    Esta es la parte final de mi parte, 11:32 empieza tu parte jorge

--- MachinLocal/MachinLocal/Models/MachinLocalDbContext.cs
+++ MachinLocal/MachinLocal/Models/MachinLocalDbContext.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
 namespace MachinLocal.Models
 {
-    public class MachinLocalDbContext
+    public class MachinLocalDbContext : DbContext
     {
+        public MachinLocalDbContext() : base("MachinLocalDbContext")
+        {
+
+        }
+
+        public DbSet<Servicios> Servicios { get; set; }
+
+        public DbSet<Citas> Citas { get; set; }
+
+        public DbSet<Sucursales> Sucursales { get; set; }
+
+        public DbSet<Clientes> Clientes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
