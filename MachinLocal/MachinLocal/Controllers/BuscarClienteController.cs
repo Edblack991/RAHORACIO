a79dfed00f1465130714cfa0fae53c80b2f9658d
edgar alfredo gonzalez salazar <piligonio@gmail.com>
2018-10-27 13:41:46 -0500

    cambio final

---
+++ MachinLocal/MachinLocal/Controllers/BuscarClienteController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MachinLocal.Controllers
+{
+    public class BuscarClienteController : Controller
+    {
+        // GET: BuscarCliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BuscarCliente/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BuscarCliente/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BuscarCliente/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BuscarCliente/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BuscarCliente/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BuscarCliente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BuscarCliente/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MachinLocal/MachinLocal/Controllers/InfoClienteController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MachinLocal.Controllers
+{
+    public class InfoClienteController : Controller
+    {
+        // GET: InfoCliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: InfoCliente/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: InfoCliente/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InfoCliente/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InfoCliente/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: InfoCliente/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InfoCliente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: InfoCliente/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
